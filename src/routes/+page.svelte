3b9afb6c9de2c6b9a8fc403231a027d8b4d04ade
Jamie Goodson <goodsonjamie@gmail.com>
2023-10-15 13:37:18 +0100

    Update prettier version to latest

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
+<script lang="ts">
+</script>
+
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="The Optimist Post" />
+	<meta name="description" content="Hello, World!" />
 </svelte:head>
 
 <section>
 	<h1>Hello, World!</h1>
 </section>
-
-<script lang="ts">
-</script>
