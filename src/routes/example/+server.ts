7927abce0e7847f12dbcd66e104cd14e0da52d1f
Jamie Goodson <goodsonjamie@gmail.com>
2023-10-15 16:23:59 +0100

    Remove kv reference, add nvmrc

--- src/routes/example/+server.ts
+++ src/routes/example/+server.ts
@@ -1,8 +1,5 @@
 import { json } from '@sveltejs/kit';
-import { kv } from '@vercel/kv';
 
 export async function GET() {
-	const data = await kv.get('testKey');
-
-	return json({ data });
+	return json({ data: 'my data' });
 }
